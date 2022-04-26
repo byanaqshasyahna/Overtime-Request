ad360831ca760844a07d226e273e4290526376a8
papahgilak05 <papahgilak@gmail.com>
2022-04-26 12:46:25 +0700

    Update Clients
    Add Button and Rearrange some items

--- OvertimeRequest_Client/Controllers/DashboardController.cs
+++ OvertimeRequest_Client/Controllers/DashboardController.cs
@@ -9,29 +9,11 @@ using System.Threading.Tasks;
 
 namespace OvertimeRequest_Client.Controllers
 {
-    public class HomeController : Controller
+    public class DashboardController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- OvertimeRequest_Client/Controllers/HomeController.cs
+++ OvertimeRequest_Client/Controllers/HomeController.cs
@@ -9,11 +9,30 @@ using System.Threading.Tasks;
 
 namespace OvertimeRequest_Client.Controllers
 {
-    public class DashboardController : Controller
+    public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
+
