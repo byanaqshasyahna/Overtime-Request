2caa8085a40cb5212ae67b45b7f2ab943f111d04
papahgilak05 <papahgilak@gmail.com>
2022-04-27 20:08:50 +0700

    Update Client
    Add Js for Approval and OvertimeList

---
+++ OvertimeRequest_Client/wwwroot/js/ApprovalFinance.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+    $("#ApprovalFinance").DataTable({
+        "filter": true,
+        "orderMulti": false,
+        "processing": true,
+        "ajax": {
+            "url": " ",
+            "datatype": "json",
+            "dataSrc": " "
+        },
+        "columns": [
+            {
+                "data": null,
+                "name": "no",
+                "autoWidth": true,
+                "render": function (data, type, row, meta) {
+                    return meta.row + 1;
+                }
+            },
+            {
+                "data": 'fullname',
+                "autoWidth": true,
+
+            },
+            {
+                "data": "role",
+                "audtoWidth": true,
+
+            },
+            {
+                "autowidth": true,
+                "orderable": false,
+                "render": function (data, type, row) {
+                    return `<button onclick="" class="btn btn-danger"></button>
+                            <button onclick="" class="btn btn-success"></button>`
+                }.
+            }]
+    });
+});
+
+function Approve() { }
+function Decline() { }
