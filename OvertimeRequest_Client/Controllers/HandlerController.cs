5fa6f5de11623f9c9e505facc39628f381c84cde
byanaqshasyahna <byanaqsha@gmail.com>
2022-05-12 22:21:04 +0700

    up from byan
    handler

---
+++ OvertimeRequest_Client/Controllers/HandlerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OvertimeRequest_API.VirtualModels;
+using OvertimeRequest_Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OvertimeRequest_Client.Controllers
+{
+    public class HandlerController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HandlerController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+    }
+}
+
