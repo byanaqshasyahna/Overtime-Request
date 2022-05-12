ef797807db17a013c792256284ab9ff4edae606b
byanaqshasyahna <byanaqsha@gmail.com>
2022-05-12 23:00:55 +0700

    up from byan

--- OvertimeRequest_Client/wwwroot/js/site.js
+++ OvertimeRequest_Client/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+var span = document.getElementById('nameLogin');
+
+while (span.firstChild) {
+    span.removeChild(span.firstChild);
+}
+span.appendChild(document.createTextNode("Byan"));
